19c622c947561134fe14878fd0cc00ab90fed16e
Amer Jazaerli <amer.jazaerly@gmail.com>
2011-12-22 04:29:57 +0200

    Acceptance test for Sign Up and Sign In sescenarios

--- spec/acceptance/dummy_spec.rb
+++ spec/acceptance/dummy_spec.rb
@@ -5,3 +5,27 @@ feature 'Dummy feature' do
     visit '/'
   end
 end
+
+feature 'Sign Up' do
+  scenario 'Sign up scenario', :js => true do
+    visit '/signup'
+    fill_in 'Name', :with => 'samer'
+    fill_in 'Login', :with => 'samer'
+    fill_in 'Email', :with => 'samer@samer.com'
+    fill_in 'Sender', :with => '009988776655'
+    fill_in 'Password', :with => '123456'
+    fill_in 'user_password_confirmation', :with => '123456'
+    click_on 'Sign Up'
+    page.should have_content('Registration successful.')
+  end
+end
+
+feature 'Login' do
+  scenario 'Login scenario', :js => true do
+    visit '/login'
+    fill_in 'Login', :with => 'samer'
+    fill_in 'Password', :with => '123456'
+    click_on 'Sign In'
+    page.should have_content('Successfully logged in.')
+  end
+end
